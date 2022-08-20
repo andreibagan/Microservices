2810c9faa167a296ea2e79d6a7b4dcd36428db77
Андрей Баган <a.bagan@beesender.com>
2022-08-20 16:52:40 +0300

    Added Event Listener

---
+++ CommandsService/AsyncDataServices/MessageBusSubscriber.cs
@@ -0,0 +1,86 @@
+﻿using CommandsService.EventProcessing;
+using CommandsService.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommandsService.AsyncDataServices
+{
+    public class MessageBusSubscriber : BackgroundService
+    {
+        private readonly RabbitMQSettings _rabbitMQSettings;
+        private readonly IEventProcessor _eventProcessor;
+        private IConnection _connection;
+        private IModel _channel;
+        private string _queueName;
+
+        public MessageBusSubscriber(IConfiguration config, IEventProcessor eventProcessor)
+        {
+            _rabbitMQSettings = config.GetSection("RabbitMQSettings").Get<RabbitMQSettings>();
+            _eventProcessor = eventProcessor;
+
+            InitializeRabbitMQ();
+        }
+
+        private void InitializeRabbitMQ()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _rabbitMQSettings.Host,
+                Port = _rabbitMQSettings.Port
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+            _queueName = _channel.QueueDeclare().QueueName;
+            _channel.QueueBind(queue: _queueName,
+                exchange: "trigger",
+                routingKey: "");
+
+            Console.WriteLine("--> Listening on the Message Bus...");
+
+            _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+        }
+
+        private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e)
+        {
+            Console.WriteLine("--> Connection Shutdown");
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(_channel);
+
+            consumer.Received += (ModuleHandle, ea) =>
+            {
+                Console.WriteLine("--> Event Received!");
+
+                var body = ea.Body;
+                var notificationMessage = Encoding.UTF8.GetString(body.ToArray());
+
+                _eventProcessor.ProcessEvent(notificationMessage);
+            };
+
+            return Task.CompletedTask;
+        }
+
+        public override void Dispose()
+        {
+            if (_channel.IsOpen)
+            {
+                _channel.Close();
+                _connection.Close();
+            }
+
+            base.Dispose();
+        }
+    }
+}
--- CommandsService/Startup.cs
+++ CommandsService/Startup.cs
@@ -1,18 +1,13 @@
+using CommandsService.AsyncDataServices;
 using CommandsService.Data;
 using CommandsService.EventProcessing;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CommandsService
 {
@@ -32,7 +27,9 @@ namespace CommandsService
             services.AddScoped<ICommandRepo, CommandRepo>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSingleton<IEventProcessor, EventProcessor>();
+            services.AddHostedService<MessageBusSubscriber>();
         }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
